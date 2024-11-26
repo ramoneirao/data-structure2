8fe9473a3db1f8a71d537da17194e3be901bb486
Ramon Mendes <ramonneirao@gmail.com>
2024-11-26 14:50:28 -0300

    add percurso simétrico

--- arvores/arvore.py
+++ arvores/arvore.py
@@ -14,13 +14,50 @@ class ArvoreBinaria:
             self.raiz = no
         else:
             self.raiz = None
+    
+    def percursoSimetrico(self, no=None):
+        if no is None:
+            no = self.raiz
+        if no.esq:
+            #print('(', end='')
+            self.percursoSimetrico(no.esq)
+        print(no)
+        
+        if no.dir:
+            self.percursoSimetrico(no.dir)
+            #print(')', end='')
+
 
 
 if __name__ == "__main__":
-    arvore = ArvoreBinaria(2)
-    arvore.raiz.esq = NoArvore(1)
-    arvore.raiz.dir = NoArvore(3)
+    #arvore = ArvoreBinaria(2)
+    #arvore.raiz.esq = NoArvore(1)
+    #arvore.raiz.dir = NoArvore(3)
 
-    print(arvore.raiz)
-    print(arvore.raiz.esq)
-    print(arvore.raiz.dir)
+    #print(arvore.raiz)
+    #print(arvore.raiz.esq)
+    #print(arvore.raiz.dir)
+
+    tree = ArvoreBinaria()
+    n1 = NoArvore('a')
+    n2 = NoArvore('+')
+    n3 = NoArvore('*')
+    n4 = NoArvore('b')
+    n5 = NoArvore('-')
+    n6 = NoArvore('/')
+    n7 = NoArvore('c')
+    n8 = NoArvore('d')
+    n9 = NoArvore('e')
+
+    n6.esq = n7
+    n6.dir = n8
+    n5.esq = n6
+    n5.dir = n9
+    n3.esq = n4
+    n3.dir = n5
+    n2.esq = n1
+    n2.dir = n3
+    
+    tree.raiz = n2
+
+    tree.percursoSimetrico()
